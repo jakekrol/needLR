19ecf3619649141c6c5a85bbce128db6fd1f9d3c
jakekrol <jdkrol351@gmail.com>
2026-07-31 10:38:39 -0600

    annotate pop freq directly from truvari output

--- src/annotate_collapsed_variants.py
+++ src/annotate_collapsed_variants.py
@@ -3,79 +3,94 @@ import argparse
 from cyvcf2 import VCF
 import os
 import pandas as pd
+from jkbiolib.variant.vcf import count_alt_samples
 
 parser = argparse.ArgumentParser(description="Annotate collapsed needLR variants")
 parser.add_argument("--input_vcf", required=True, help="Path to the input VCF file")
 parser.add_argument("--collapsed_vcf", required=True, help="Path to the collapsed VCF file")
-parser.add_argument("--table", required=True, help="Path to the needLR output table")
+parser.add_argument("--merged_vcf", required=True, help="Path to the merged VCF file")
 parser.add_argument("--out", required=True, help="Output file")
 args = parser.parse_args()
 
-COL_SVID='SVID'
-COL_POP_FREQ='Pop_Freq_ALL'
-COL_COLLAPSE_ID='CollapseId'
-INVALID_POP_FREQ=-1
+COLLAPSE_ID_FIELD = "CollapseId"
+MATCH_ID_FIELD = "MatchId"
+INVALID_POP_FREQ = -1
 
+# map diagram
+# merge SVID > CollapseId = MatchId
+#      v         v            ^
+# Population frequency    Collapse SVID
 
 def main():
-    # inputs
-    df_needlr = pd.read_csv(args.table, sep="\t")
+    print("# reading vcfs")
     vcf_input = VCF(args.input_vcf)
     vcf_collapsed = VCF(args.collapsed_vcf)
+    vcf_merged = VCF(args.merged_vcf)
 
-    # gather input SVIDs
-    svids_query = set(v.ID for v in vcf_input if v.ID is not None)
-    df_query = pd.DataFrame([(svid) for svid in svids_query], columns=[COL_SVID])
+    print("# mapping merge svid to collapse id")
+    merge_svid2collapseid = {}
+    for v in vcf_merged:
+        collapseid = str(v.INFO.get(COLLAPSE_ID_FIELD)) if v.INFO.get(COLLAPSE_ID_FIELD) is not None else str(-1)
+        if ';' in v.ID:
+            svids = v.ID.split(';')
+            for svid in svids:
+                merge_svid2collapseid[svid] = collapseid
+        else:
+            merge_svid2collapseid[v.ID] = collapseid
 
-    # get SVID, Pop_Freq_ALL, and Collapse_Id in final output
-    df_needlr=df_needlr[[COL_SVID, COL_POP_FREQ, COL_COLLAPSE_ID]]
-    collapse_ids = set(df_needlr[COL_COLLAPSE_ID].unique())
-    # rm '.' which is the case where the variant was not collapsed
-    collapse_ids.discard('.')
+    print("# counting control samples with alt alleles")
+    df_merged, num_control_samples = count_alt_samples(args.merged_vcf)
 
-    # get SVID and MatchId from collapsed VCF
-    # MatchId is a key shared with Collapse_Id used to resolve variant merging
-    collapsed_data = []
-    for v in vcf_collapsed:
-        svid = v.ID if v.ID is not None else '.'
-        # we only care about query SVs, not those from their reference set which were collapsed
-        if svid in svids_query:
-            match_id = v.INFO.get('MatchId') if v.INFO.get('MatchId') is not None else '.'
-            if match_id in collapse_ids:
-                mask = df_needlr[COL_COLLAPSE_ID] == match_id
-                pop_freq = df_needlr.loc[mask, COL_POP_FREQ].values[0] if mask.any() else INVALID_POP_FREQ
-                collapsed_data.append((svid, pop_freq))
-            else:
-                collapsed_data.append((svid, match_id, INVALID_POP_FREQ))
-    df_collapsed = pd.DataFrame(collapsed_data, columns=[COL_SVID, COL_POP_FREQ])
-
-    # get SVID and Pop_Freq_ALL from needLR output table
-    needlr_data=[]
-    for i, row in df_needlr.iterrows():
-        svid = row[COL_SVID]
-        # needlr stores the SVIDs as semicolon delimited strings, so we need to split them and check each one
+    print("# mapping merge svid to pop_freq")
+    merge_svid2popfreq = {}
+    for i, row in df_merged.iterrows():
+        svid = str(row['SVID'])
+        pop_freq = row['Alt_Sample_Count'] / num_control_samples
         if ';' in svid:
             svids = svid.split(';')
             for svid in svids:
-                if svid in svids_query:
-                    pop_freq = row[COL_POP_FREQ] if row[COL_POP_FREQ] is not None else INVALID_POP_FREQ
-                    needlr_data.append((svid, pop_freq))
+                merge_svid2popfreq[svid] = pop_freq
         else:
-            if svid in svids_query:
-                pop_freq = row[COL_POP_FREQ] if row[COL_POP_FREQ] is not None else INVALID_POP_FREQ
-                needlr_data.append((svid, pop_freq))
-    df_needlr_filtered = pd.DataFrame(needlr_data, columns=[COL_SVID, COL_POP_FREQ])
-    # combine the two dataframes and remove duplicates
-    df_concat = pd.concat([df_collapsed, df_needlr_filtered], axis=0, ignore_index=True)
-    df_concat.drop_duplicates(subset=[COL_SVID], inplace=True)
-    # finally force any missing query SVIDs to have an invalid Pop_Freq_ALL value
-    final_svids = set(df_concat[COL_SVID].unique())
-    for svid in svids_query:
-        if svid not in final_svids:
-            df_concat = pd.concat([df_concat, pd.DataFrame([(svid, INVALID_POP_FREQ)], columns=[COL_SVID, COL_POP_FREQ])], axis=0, ignore_index=True)
-    df_concat.sort_values(by=[COL_POP_FREQ], inplace=True, ascending=False)
-    df_concat.to_csv(args.out, sep="\t", index=False)
+            merge_svid2popfreq[svid] = pop_freq
+
+    collapseid2popfreq = {}
+    print("# mapping CollapseId to pop_freq")
+    for svid, collapseid in merge_svid2collapseid.items():
+        pop_freq = merge_svid2popfreq.get(svid, INVALID_POP_FREQ)
+        if collapseid in collapseid2popfreq:
+            pass
+        else:
+            collapseid2popfreq[collapseid] = pop_freq
     
+    # matchid of collapsed vcf is a key equivalent to matchid of the merged vcf
+    print("# mapping collapsed svids -> population frequency")
+    collapsed_svids2popfreq = {}
+    for v in vcf_collapsed:
+        match_id = str(v.INFO.get(MATCH_ID_FIELD)) if v.INFO.get(MATCH_ID_FIELD) is not None else str(-1)
+        pop_freq = collapseid2popfreq.get(match_id, INVALID_POP_FREQ)
+        if ';' in v.ID:
+            svids = v.ID.split(';')
+            for svid in svids:
+                collapsed_svids2popfreq[svid] = pop_freq 
+        else:
+            collapsed_svids2popfreq[v.ID] = pop_freq
+
+    print("# getting population frequency for query SVIDs")
+    outdata=[]
+    svids_query = set(v.ID for v in vcf_input if v.ID is not None)
+    for svid in svids_query:
+        if svid in collapsed_svids2popfreq:
+            pop_freq = collapsed_svids2popfreq[svid]
+        elif svid in merge_svid2popfreq:
+            pop_freq = merge_svid2popfreq[svid]
+        else:
+            pop_freq = INVALID_POP_FREQ
+        outdata.append((svid, pop_freq))
+    df = pd.DataFrame(outdata, columns=['svid', 'population_frequency'])
+    df = df.sort_values(by='population_frequency', ascending=False)
+    print("# writing output to {}".format(args.out))
+    df.to_csv(args.out, sep="\t", index=False)
+
 
 if __name__ == "__main__":
     main()
